35f01d639aa03602d2ed8ac2d0127b05ed52b692
Alasdair McLeay <alasdair.mcleay@me.com>
2023-01-24 22:04:56 +0000

    add required dependencies from remix monorepo

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  ...require("../../jest/jest.config.shared"),
+  ...require("./jest/jest.config.shared"),
   displayName: "google-cloud-functions",
 };
