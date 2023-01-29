14d79818b53bcbc428bfc9e080394d5d826643d7
Alasdair McLeay <alasdair.mcleay@me.com>
2023-01-29 01:15:52 +0000

    Longer timeout on initial expect

--- example/__tests__/app.spec.ts
+++ example/__tests__/app.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test("all the things", async ({ page }) => {
   await page.goto("http://127.0.0.1:5002/");
-  await expect(page).toHaveTitle("Remix + Firebase");
+  await expect(page).toHaveTitle("Remix + Firebase", {
+    timeout: 20000,
+  });
   await page.getByPlaceholder("you@example.com").fill("user@example.com");
   await page.getByPlaceholder("password").fill("password");
   await page.getByRole("button", { name: "Login" }).click();
